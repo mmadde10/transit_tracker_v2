462fd155f59ab168a54f19808c6aefd8aaeef247
Mark Madden <mark.madden@rate.com>
2018-09-23 15:13:09 -0500

    cleanup

--- lib/widgets/Lists/RedLineList.dart
+++ lib/widgets/Lists/RedLineList.dart
@@ -7,286 +7,73 @@ class RedLineListView extends StatelessWidget{
     return new Container(
      child:ListView(
           children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.train),
-                trailing: Icon(Icons.accessible),
-                title: Text("Howard"),
-                subtitle: Row(
-                  children: <Widget>[
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Yellow"],
-                    ),
-                    new Icon(
-                    Icons.train,
-                    size: 20.0,
-                    color: lineColors["Purple"],
-                    )
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Jarvis"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Morse"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Loyola"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Granville"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Thorndale"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Bryn Mawr"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Argyle"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Lawrence"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Wilson"),
-                trailing: Icon(Icons.accessible),
-                subtitle: Row(
-                  children: <Widget>[
-                    new Icon(
-                    Icons.train,
-                    size: 20.0,
-                    color: lineColors["Purple"],
-                    )
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Sheridan"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Addison"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Belmont"),
-                trailing: Icon(Icons.accessible),
-                subtitle: Row(
-                  children: <Widget>[
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Brown"],
-                    ),
-                    new Icon(
-                    Icons.train,
-                    size: 20.0,
-                    color: lineColors["Purple"],
-                    )
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Fullerton"),
-                trailing: Icon(Icons.accessible),
-                subtitle: Row(
-                  children: <Widget>[
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Brown"],
-                    ),
-                    new Icon(
-                    Icons.train,
-                    size: 20.0,
-                    color: lineColors["Purple"],
-                    )
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("North/Clybourn"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Clark/Division"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Chicago"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Grand"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Lake"),
-                trailing: Icon(Icons.accessible),
-                subtitle: Row(
-                  children: <Widget>[
-                    new Icon(
-                        Icons.train,
-                        size: 20.0,
-                        color: lineColors["Brown"],
-                      ),
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Green"],
-                      ),
-                    new Icon(
-                        Icons.train,
-                        size: 20.0,
-                        color: lineColors["Orange"],
-                      ),
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Pink"],
-                      ),
-                    new Icon(
-                        Icons.train,
-                        size: 20.0,
-                        color: lineColors["Purple"],
-                      ),
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Blue"],
-                      )
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Monroe"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Jackson"),
-                trailing: Icon(Icons.accessible),
-                subtitle: Row(
-                  children: <Widget>[
-                    new Icon(
-                        Icons.train,
-                        size: 20.0,
-                        color: lineColors["Brown"],
-                      ),
-                    new Icon(
-                        Icons.train,
-                        size: 20.0,
-                        color: lineColors["Orange"],
-                      ),
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Pink"],
-                      ),
-                    new Icon(
-                        Icons.train,
-                        size: 20.0,
-                        color: lineColors["Purple"],
-                      ),
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Blue"],
-                      )
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Harrison"),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Harrison"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Roosevelt"),
-                trailing: Icon(Icons.accessible),
-                subtitle: Row(
-                  children: <Widget>[
-                    new Icon(
-                      Icons.train,
-                      size: 20.0,
-                      color: lineColors["Orange"],
-                    ),
-                    new Icon(
-                    Icons.train,
-                    size: 20.0,
-                    color: lineColors["Green"],
-                    )
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Cermak-Chinatown"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Sox-35th"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("47th"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("Garfield"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("63rd"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("69th"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("87th"),
-                trailing: Icon(Icons.accessible),
-              ),
-              ListTile(
-                leading: Icon(Icons.train),
-                title: Text("95th/Dan Ryan"),
-                trailing: Icon(Icons.accessible),
-              ),
+            RedLineListItem("Howard",true,["Yellow","Purple"]),
+            RedLineListItem("Jarvis",true,[]),
+            RedLineListItem("Morse",false,[]),
+            RedLineListItem("Loyola",true,[]),
+            RedLineListItem("Granville",false,[]),
+            RedLineListItem("Thorndale",false,[]),
+            RedLineListItem("Bryn Mawr",true,[]),
+            RedLineListItem("Argyle",false,[]),
+            RedLineListItem("Lawrence",false,[]),
+            RedLineListItem("Wilson",true,["Purple"]),
+            RedLineListItem("Sheridan",false,[]),
+            RedLineListItem("Addison",true,[]),
+            RedLineListItem("Belmont",true,["Brown","Purple"]),
+            RedLineListItem("Fullerton",true,["Brown","Purple"]),
+            RedLineListItem("North/Clybourn",false,[]),
+            RedLineListItem("Clark/Division",true,[]),
+            RedLineListItem("Chicago",true,[]),
+            RedLineListItem("Grand",true,[]),
+            RedLineListItem("Lake",true,["Brown", "Green", "Orange", "Pink", "Purple","Blue"]),
+            RedLineListItem("Monroe",true,[]),
+            RedLineListItem("Jackson",true,["Brown", "Orange", "Pink", "Purple","Blue"]),
+            RedLineListItem("Harrison",true,[]),
+            RedLineListItem("Roosevelt",true,["Orange", "Green"]),
+            RedLineListItem("Cermak-Chinatown",true,[]),  
+            RedLineListItem("Sox-35th",true,[]), 
+            RedLineListItem("47th",true,[]),
+            RedLineListItem("Garfield",true,[]),
+            RedLineListItem("63rd",true,[]),
+            RedLineListItem("69th",true,[]), 
+            RedLineListItem("87th",true,[]),
+            RedLineListItem("95th/Dan Ryan",true,[])
           ],
         ),
     );
+  }
+}
+
+class RedLineListItem extends StatelessWidget{
+  final String stationName;
+  final bool isAccessible;
+  final List transferColors;
+
+  RedLineListItem(this.stationName,this.isAccessible,this.transferColors);
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      leading: Icon(Icons.train),
+      trailing: renderAccesable(isAccessible),
+      title: new Text(stationName),
+      subtitle: renderTransfers(transferColors)
+    );
+  }
+  renderAccesable(isAccessible){
+    if(isAccessible){
+      return Icon(Icons.accessible);
+    }
+  }
+  Widget renderTransfers(transferColors){
+     List<Widget> list = new List<Widget>();
+     for(var i = 0; i < transferColors.length; i++){
+        list.add(
+          new Icon(
+            Icons.train,
+            size: 20.0,
+            color: lineColors[transferColors[i]]));
+    }
+    return new Row(children: list);
   }
 }
